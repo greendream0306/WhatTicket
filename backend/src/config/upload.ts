8a99812560c6785e281977f0ff25cbe5b742a1b4
Green Dream <101880509+greendream0306@users.noreply.github.com>
2022-05-15 06:54:13 +0800

    backend init

---
+++ backend/src/config/upload.ts
@@ -0,0 +1,17 @@
+import path from "path";
+import multer from "multer";
+
+const publicFolder = path.resolve(__dirname, "..", "..", "public");
+
+export default {
+    directory: publicFolder,
+
+    Storage: multer.diskStorage({
+        destination: publicFolder,
+        filename(req, file, cb) {
+            const fileName = new Date().getTime() + path.extname(file.originalname);
+
+            return cb(null, fileName);
+        }
+    })
+};
